60d089988744e51c6f1c19eda56ba5e133065816
kopollo <andreysamsaa@gmail.com>
2023-04-20 18:29:58 +0300

    add mock db session and db tests

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 """Entrance point of program."""
-from db import db_session
 from bot_handler import dp_handlers
-from food_api_handler.food_searcher import FoodApiManager
+from db import db_session
+
 if __name__ == "__main__":
     db_session.global_init('db/dish_finder.db')
     dp_handlers.run()
--- tests/test_db.py
+++ tests/test_db.py
@@ -1,51 +1,44 @@
-# import pytest
-# from sqlalchemy import create_engine
-# from sqlalchemy.orm import scoped_session, sessionmaker
-#
-#
-# @pytest.fixture(scope='session')
-# def db_engine():
-#     """yields a SQLAlchemy engine which is suppressed after the test session"""
-#     db_file = "test_dish_finder"
-#     db_url = f'sqlite:///{db_file.strip()}?check_same_thread=False'
-#     engine_ = create_engine(db_url, echo=True)
-#     yield engine_
-#     engine_.dispose()
-#
-#
-# @pytest.fixture(scope='session')
-# def db_session_factory(db_engine):
-#     """returns a SQLAlchemy scoped session factory"""
-#     return scoped_session(sessionmaker(bind=db_engine))
-#
-#
-# @pytest.fixture(scope='function')
-# def db_session(db_session_factory):
-#     """yields a SQLAlchemy connection which is rollbacked after the test"""
-#     session_ = db_session_factory()
-#
-#     yield session_
-#
-#     session_.rollback()
-#     session_.close()
-#
-# # def test_add_dish(self):
-# #     db_sess = db_session.create_session()
-# #     title = 'apple'
-# #     instruction = 'text tet xt t et \n te x'
-# #     idx = "1245"
-# #     dish = Dish(
-# #         id=idx,
-# #         title=title,
-# #         # image_url="/fsdfsdfs",
-# #         instruction=instruction,
-# #     )
-# #     db_sess.merge(dish)
-# #     db_sess.commit()
-# #
-# #
-# # def test_print_dish(self):
-# #     db_sess = db_session.create_session()
-# #     dish = db_sess.query(Dish).all()
-# #     for i in dish:
-# #         print(i)
+import pytest
+from sqlalchemy.exc import IntegrityError
+
+
+def test_add_then_get_correct_dish(mock_db_manager, correct_dish):
+    mock_db_manager.add_dish(correct_dish)
+    ans = mock_db_manager.get_dish(1)
+    assert ans.title == 'x' and ans.instruction == 'x'
+
+
+@pytest.mark.xfail(raises=IntegrityError)
+def test_add_incorrect_dish(mock_db_manager, incorrect_dish):
+    mock_db_manager.add_dish(incorrect_dish)
+
+
+def test_add_then_get_correct_user(mock_db_manager, correct_user):
+    mock_db_manager.add_user(correct_user)
+    ans = mock_db_manager.get_user(1)
+    assert ans.tg_id == 1
+
+
+@pytest.mark.xfail(raises=IntegrityError)
+def test_add_incorrect_user(mock_db_manager, incorrect_user):
+    mock_db_manager.add_user(incorrect_user)
+
+
+def test_get_all_user_dishes(mock_db_manager, correct_user):
+    mock_db_manager.add_user(correct_user)
+    ans = mock_db_manager.get_all_user_dishes(1)
+    assert ans == []
+
+
+def test_save_dish_event_then_get_it(mock_db_manager, correct_dish,
+                                     correct_user):
+    mock_db_manager.save_dish_event(correct_dish, correct_user)
+    ans = mock_db_manager.get_all_user_dishes(1)[0]
+    assert ans.title == 'x'
+
+
+@pytest.mark.xfail(raises=IndexError)
+def test_save_dish_event_with_wrong_idx(mock_db_manager, correct_dish,
+                                        correct_user):
+    mock_db_manager.save_dish_event(correct_dish, correct_user)
+    ans = mock_db_manager.get_all_user_dishes(1)[23]
