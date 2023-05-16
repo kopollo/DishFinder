ada066c7fef0ac01d5466c1b3afe1813932e7801
kopollo <andreysamsaa@gmail.com>
2023-05-17 02:35:41 +0300

    refactor connection to DB in port-adapter view

--- bot_handler/facades.py
+++ bot_handler/facades.py
@@ -3,69 +3,38 @@ from dataclasses import asdict
 from typing import Optional
 
 from bot_handler.bot_context import DishInBotRepr, TelegramUser
-from db.db_manager import DBManager
-from db import DishModel, UserModel
+# from db.db_manager import DBManager
+# from db import DishModel, UserModel
 from food_api_handler.food_searcher import FoodApiManager
 from food_api_handler.requests_wrapper import DishApiRepr
+from db.adapter import db_adapter as adapter
 
-db_manager = DBManager()
+
+# db_manager = DBManager()
 
 
-class DBFilter:
+class StoragePort:
     """Facade for DB package."""
-
-    def _to_dish_in_bot_repr(self, dish: DishModel) -> DishInBotRepr:
-        dish_repr = DishInBotRepr(
-            id=dish.id,
-            title=dish.title,
-            image_url=dish.image_url,
-            ingredients=dish.ingredients,
-            instruction=dish.instruction,
-        )
-        return dish_repr
-
-    def _to_dish_model(self, dish: DishInBotRepr) -> DishModel:
-        dish_repr = DishModel(**asdict(dish))
-        return dish_repr
 
     def get_dish(self, dish_id: int) -> DishInBotRepr:
         """Return a dish in DB by id and convert it to DishInBotRepr."""
-        return self._to_dish_in_bot_repr(db_manager.get_dish(dish_id))
+        return adapter.get_dish(dish_id)
 
     def save_dish(self, dish: DishInBotRepr, user: TelegramUser) -> None:
         """Save a dish to user in DB."""
-        db_manager.save_dish_event(self._to_dish_model(dish),
-                                   self._to_user_model(user))
+        return adapter.save_dish(dish, user)
 
     def get_user_dishes(self, user_id: int) -> list[DishInBotRepr]:
         """Return dishes in DB by user id."""
-        dishes = db_manager.get_all_user_dishes(user_id)
-        dishes = [self._to_dish_in_bot_repr(i) for i in dishes]
-        return dishes
-
-    def _to_telegram_user(self, user_model: UserModel) -> TelegramUser:
-        # TODO write to_dict for DB models
-        user = TelegramUser(
-            first_name=user_model.first_name,
-            last_name=user_model.last_name,
-            tg_id=user_model.tg_id,
-            language=user_model.language,
-        )
-        return user
+        return adapter.get_user_dishes(user_id)
 
     def add_user(self, user: TelegramUser) -> None:
         """Add user to DB."""
-        db_manager.add_user(self._to_user_model(user))
+        adapter.add_user(user)
 
     def get_user(self, user_id) -> Optional[TelegramUser]:
         """Return user in DB, if user exist and None otherwise."""
-        user_model = db_manager.get_user(user_id)
-        if not user_model:
-            return None
-        return self._to_telegram_user(user_model)
-
-    def _to_user_model(self, tg_user: TelegramUser) -> Optional[UserModel]:
-        return UserModel(**asdict(tg_user))
+        return adapter.get_user(user_id)
 
 
 class DishSearchFilter:
--- bot_handler/setup.py
+++ bot_handler/setup.py
@@ -5,11 +5,11 @@ from aiogram import Bot, Dispatcher
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 import logging
 
-from bot_handler.facades import DBFilter
+from bot_handler.facades import StoragePort
 
 TELEGRAM_TOKEN = os.environ.get('TELEGRAM_TOKEN')
 bot = Bot(TELEGRAM_TOKEN)
 storage = MemoryStorage()
 dp = Dispatcher(bot, storage=storage)
 logging.basicConfig(level=logging.ERROR)
-db_filter = DBFilter()
+db_filter = StoragePort()
